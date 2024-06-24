cfa7b92e5b033b6aa68deb5d96c5ece1ac000b39
lionjeongin <cindy93489163@gmail.com>
2024-06-24 14:24:49 +0900

    Maincontroller / index.jsp / style.css Mincontroller + 메인페이지 생성 + 스타일 생성

---
+++ src/main/java/org/choongang/main/controllers/MainController.java
@@ -0,0 +1,17 @@
+package org.choongang.main.controllers;
+
+import org.choongang.global.config.annotations.Controller;
+import org.choongang.global.config.annotations.GetMapping;
+import org.choongang.global.config.annotations.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class MainController {
+
+    @GetMapping
+    public String index() {
+
+        return "main/index";
+    }
+}
+// webapp-WEB-INF-template-main-index.jsp
